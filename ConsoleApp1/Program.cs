0b792919bbaab8e3045b3b95a5051dc8d85a5477
QATesterDevOps <69948902+QATesterDevOps@users.noreply.github.com>
2020-08-23 20:17:28 -0500

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jorge!, ingrese los numeros correspondientes para realizar la suma. ");
+            Console.WriteLine("Ingrese el primer numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int suma = a + b;
+            Console.WriteLine("La suma es: "+suma) ;
+        }
+    }
+}
